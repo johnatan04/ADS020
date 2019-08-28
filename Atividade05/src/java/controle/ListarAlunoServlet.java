584a0ca3cbe1f7353a349f33fcfcb4d91cd0b483
18114290048 <18114290048@OLAB04-D09.academico.iesb.br>
2019-08-27 21:45:33 -0300

    Atividade05 em progresso

--- Atividade05/src/java/controle/ListarAlunoServlet.java
+++ Atividade05/src/java/controle/ListarAlunoServlet.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import modelo.AlunoBO;
-import modelo.BO;
 import modelo.NegocioException;
 import modelo.entidades.Aluno;
 
@@ -20,7 +19,7 @@ public class ListarAlunoServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        
+
         try (PrintWriter out = response.getWriter()) {
 
             out.println("<!DOCTYPE html>");
@@ -40,13 +39,47 @@ public class ListarAlunoServlet extends HttpServlet {
             throws ServletException, IOException {
         List<Aluno> lista = new ArrayList<Aluno>();
         AlunoBO bo = new AlunoBO();
-        
+        // tipo variavel e new; 
+
         try {
             lista = bo.listar();
         } catch (NegocioException e) {
             throw new ServletException("", e);
-            
+
         }
+        try (PrintWriter out = response.getWriter()) {
+
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Cadastro de Alunos</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Listagem de Alunos</h1>");
+            out.println("</table>");
+            out.println("</tr>");
+            out.println("<th>Matrícula</th>");
+            out.println("<th>Nome</th>");
+            out.println("<th>Ações</th>");
+            out.println("</tr>");
+            for(Aluno aluno: lista) {
+  out.println("<tr>");
+  out.println("<td>" + aluno.getMatricula() + "</td>");
+  out.println("<td>" + aluno.getMatricula() + "</td>");
+  out.println("<td><a href=''>Alterar</a>");
+  out.println("<a href=''>Excluir</a></td>");
+  out.println("</tr>");
+}
+            out.println("</table>");
+            out.println("<body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp); 
+        
     }
 
     @Override
